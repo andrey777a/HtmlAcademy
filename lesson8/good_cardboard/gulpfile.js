c5c5391893af074e021f84d13889051963e3764a
andrey777a <52427993+andrey777a@users.noreply.github.com>
2021-01-23 13:40:19 +0300

    add les8 and les9
    project on gulp and flex

---
+++ lesson8/good_cardboard/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch("src/sass/*.sass", ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("src/sass/*.sass")
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
